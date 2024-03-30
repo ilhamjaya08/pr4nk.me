7701892cffa6e5d321ca1881e26da070e5a0d392
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-03-30 23:04:39 +0800

    🔥init: navbar&footer;

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -357,11 +357,10 @@ const Navbar = () => {
                           </svg>
                           <div className="ml-4">
                             <p className="text-base font-medium text-gray-900">
-                              Guides
+                              Issues
                             </p>
                             <p className="mt-1 text-sm text-gray-500">
-                              Learn how to maximize our platform to get the most
-                              out of it.
+                              Find Bugs or System error on this website and report it to us, and help us find other problems!
                             </p>
                           </div>
                         </a>
@@ -387,11 +386,10 @@ const Navbar = () => {
                           </svg>
                           <div className="ml-4">
                             <p className="text-base font-medium text-gray-900">
-                              Events
+                              Contribute
                             </p>
                             <p className="mt-1 text-sm text-gray-500">
-                              See what meet-ups and other events we might be
-                              planning near you.
+                              Contribute to this web project by contacting us and helping us in developing this project.
                             </p>
                           </div>
                         </a>
@@ -473,17 +471,17 @@ const Navbar = () => {
                 </div>
               </nav>
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a
+                {/*<a
                   href="#"
                   className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
                 >
                   Sign in
-                </a>
+                  </a>*/}
                 <a
                   href="#"
                   className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  Sign up
+                  Submit your own pranks!
                 </a>
               </div>
             </div>
@@ -548,120 +546,50 @@ const Navbar = () => {
                       href="#"
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      {/* Heroicon name: outline/chart-bar */}
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-                        />
-                      </svg>
+                      {/* fa6: FaComputer */}
+                      <FaComputer className="flex-shrink-0 h-6 w-6 text-indigo-600" />
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Analytics
+                        Simulator
                       </span>
                     </a>
                     <a
                       href="#"
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      {/* Heroicon name: outline/cursor-click */}
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-                        />
-                      </svg>
+                      {/* react-icons: gr GrSystem */}
+                      <GrSystem className="flex-shrink-0 h-6 w-6 text-indigo-600" />
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Engagement
+                        Fake Update
                       </span>
                     </a>
                     <a
                       href="#"
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      {/* Heroicon name: outline/shield-check */}
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-                        />
-                      </svg>
+                      {/* react-icons: ri RiGhost2Fill */}
+                      <RiGhost2Fill className="flex-shrink-0 h-6 w-6 text-indigo-600" />
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Security
+                        Jumpscares
                       </span>
                     </a>
                     <a
                       href="#"
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      {/* Heroicon name: outline/view-grid */}
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-                        />
-                      </svg>
+                      {/* react-icons: si SiTryhackme */}
+                      <SiTryhackme className="flex-shrink-0 h-6 w-6 text-indigo-600" />
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Integrations
+                        Fake Hacker
                       </span>
                     </a>
                     <a
                       href="#"
                       className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
                     >
-                      {/* Heroicon name: outline/refresh */}
-                      <svg
-                        className="flex-shrink-0 h-6 w-6 text-indigo-600"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                        />
-                      </svg>
+                      {/* react-icons: md MdOtherHouses */}
+                          <MdOtherHouses className="flex-shrink-0 h-6 w-6 text-indigo-600" />
                       <span className="ml-3 text-base font-medium text-gray-900">
-                        Automations
+                        Other Pranks
                       </span>
                     </a>
                   </nav>
@@ -673,19 +601,19 @@ const Navbar = () => {
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    Pricing
+                    Contribute
                   </a>
                   <a
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    Docs
+                    About
                   </a>
                   <a
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    Enterprise
+                    Changelogs
                   </a>
                   <a
                     href="#"
@@ -709,13 +637,13 @@ const Navbar = () => {
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    Security
+                    Disclaimer
                   </a>
                   <a
                     href="#"
                     className="text-base font-medium text-gray-900 hover:text-gray-700"
                   >
-                    Events
+                    Bug Reports
                   </a>
                 </div>
                 <div>
@@ -723,13 +651,10 @@ const Navbar = () => {
                     href="#"
                     className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                   >
-                    Sign up
+                    Submit your own Pranks!
                   </a>
                   <p className="mt-6 text-center text-base font-medium text-gray-500">
-                    Existing customer?
-                    <a href="#" className="text-indigo-600 hover:text-indigo-500">
-                      Sign in
-                    </a>
+                    or contact us for more informations
                   </p>
                 </div>
               </div>
