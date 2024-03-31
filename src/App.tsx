ec29c99f45121867bcb3c05b0a2f5a2adbd72222
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-04-01 00:22:12 +0800

    init: Home Page;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import { HashRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
 
 function App() {
   return (
        <>
-         <Navbar />
-         <Footer />
+         <HashRouter>
+          <Routes>
+            <Route path="/">
+              <Route index element={<Home />} />
+            </Route>
+          </Routes>
+         </HashRouter>
        </>
   );
 }
